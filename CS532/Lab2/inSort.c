f2f0ba9c5e8fefc48770c797bfb7194d5c6577e9
silviarenault <150359526+silviarenault@users.noreply.github.com>
2026-06-14 23:43:09 +0000

    Insertion sort fixed

--- CS532/Lab2/inSort.c
+++ CS532/Lab2/inSort.c
@@ -4,14 +4,35 @@ int insertionSort(int N){
     int arr[N];
     int i;
     printf("Give %d integers to add to the array:\n", N);
-    for(i=0; i<N; i++)
+    for(i=0; i<N; i++){
         scanf("%d", &arr[i]);
+    }
     printf("The original array is: [%d,", arr[0]);
-    for(i=1; i<N; i++)
+    for(i=1; i<N; i++){
         if(i==N-1)
-            printf(" %d]", arr[i]);
+            printf(" %d]\n", arr[i]);
         else
             printf(" %d,", arr[i]);
+
+    }
+    int j;
+    int temp;
+    for(i=1; i<N; i++){
+        for(j=i; j>0; j--){
+            if(arr[j-1]>arr[j]){
+                temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+                }
+            }
+    }
+    printf("The sorted array is: [%d,", arr[0]);
+    for(i=1; i<N; i++){
+        if(i==N-1)
+            printf(" %d]\n", arr[i]);
+        else
+            printf(" %d,", arr[i]);
+    }
     return 0;
 }
 
