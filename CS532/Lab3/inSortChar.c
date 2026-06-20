cd6ea8f6ef7a5456c62bcd747defb589b67268cf
silviarenault <150359526+silviarenault@users.noreply.github.com>
2026-06-20 21:16:47 +0000

    Pointers working

--- CS532/Lab3/inSortChar.c
+++ CS532/Lab3/inSortChar.c
@@ -1,50 +1,53 @@
 #include <stdio.h>
 #include <string.h>
-//if the array of the first string you concatonate is too small for the added strings it will cut off, allocate more space than anticipated
+#include <stdlib.h>
 
-int insertionSort(char *arr, int N){
-    char *arr=(char*)malloc(N*sizeof(char));
-    printf("The original array is: [%d,", arr[0]);
-    for(i=1; i<N; i++){
-        if(i==N-1)
-            printf(" %c]\n", arr[i]);
+void insertionSort(char **arr, int size){
+    printf("The original array is: [%s,", arr[0]);
+    for(int i=1; i<size; i++){
+        if(i==size-1)
+            printf(" %s]\n", arr[i]);
         else
-            printf(" %c,", arr[i]);
+            printf(" %s,", arr[i]);
 
     }
-
-    printf("%d\n", strcmp(arr[0],arr[1]));
-    printf("%d\n", strcmp(arr[1],arr[0]));
-   /* int j, temp;
-    for(i=1; i<N; i++){
+    int j;
+    char *temp;
+    for(int i=1; i<size; i++){
         for(j=i; j>0; j--){
-            if(arr[j-1]>arr[j]){
+            if(strcmp(arr[j-1],arr[j])>0){
                 temp=arr[j-1];
                 arr[j-1]=arr[j];
                 arr[j]=temp;
                 }
             }
-    } 
-    printf("The sorted array is: [%d,", arr[0]);
-    for(i=1; i<N; i++){
-        if(i==N-1)
-            printf(" %d]\n", arr[i]);
+    }
+    printf("The sorted array is: [%s,", arr[0]);
+    for(int i=1; i<size; i++){
+        if(i==size-1)
+            printf(" %s]\n", arr[i]);
         else
-            printf(" %d,", arr[i]);
-    } */
-    free();
-    return 0;
+            printf(" %s,", arr[i]);
+    }
 }
 
-int main(int N){
+int main(){
+    int N;
     printf("Give a number of elements for the array\n");
     scanf("%d", &N);
-    char arr[N];
-    int i;
-    printf("Give %d integers to add to the array:\n", N);
-    for(i=0; i<N; i++){
-        scanf("%d", &arr[i]);
+    printf("Please enter the words you would like to sort\n");
+    char **a=malloc(N*sizeof(char*));
+    for(int i=0; i<N; i++){
+        char temp[100];
+        scanf("%99s", temp);
+        a[i]=malloc((strlen(temp)+1)*sizeof(char));
+        strcpy(a[i], temp);
     }
-    insertionSort(arr, N);
+    
+    insertionSort(a, N);
+    for(int i=0; i<N; i++){
+        free(a[i]);
+    } 
+    free(a);
     return 0;
-}
+} 
